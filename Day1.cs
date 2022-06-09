2c11ea023f03d548a89d37bd41f4e1d5a7a77721
Lucas Clark <47795887+encryptedsalad@users.noreply.github.com>
2022-06-09 15:39:15 -0500

    Add files via upload

---
+++ Day1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+// my first C# program!
+class Day1 {
+    static void Main(String[] args) {
+        string input = System.IO.File.ReadAllText($"./{args[0]}");
+        string[] lines = input.Split("\n");
+        int[] nums = new int[lines.Length];
+        int numgreater = 0;        
+        nums[0] = Int32.Parse(lines[0]);
+        for (int i = 1; i < lines.Length; i++) {
+            nums[i] = Int32.Parse(lines[i]);
+            if (nums[i] > nums[i-1]) {
+                numgreater++;
+            }
+        }
+        System.Console.WriteLine(numgreater);
+    }
+}
